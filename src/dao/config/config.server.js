013b7f82ca8b32d8ebe3cc63d1c9d4f699d1d19b
IanBerak <jeanrios892@gmail.com>
2023-11-22 00:25:20 -0500

    cambios

--- src/dao/config/config.server.js
+++ src/dao/config/config.server.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 // const MONGOURL = "mongodb+srv://ian-admin:admin@cluster1.9ex9tkc.mongodb.net/ecommerce?retryWrites=true&w=majority" //mongoATLAS
-const URL = "mongodb://127.0.0.1:27017/ecommerce" //LOCALHOTS 
+const URL = "mongodb://127.0.0.1:27017/ecommerce" //LOCALHOST 
 
 const connectedDB = () => {
     try {
